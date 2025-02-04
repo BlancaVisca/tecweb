bdfa03ef96df609302e7f0308163aba7895f3857
flor <blancavisca05@gmail.com>
2025-02-04 02:02:55 -0600

    Ejercicio 1

---
+++ practicas/p04/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Práctica 3</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        $_myvar;
+        $_7var;
+        //myvar;       // Inválida
+        $myvar;
+        $var7;
+        $_element1;
+        //$house*5;     // Invalida
+        
+        echo '<h4>Respuesta:</h4>';   
+    
+        echo '<ul>';
+        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
+        echo '<li>$myvar es válida porque inicia con una letra.</li>';
+        echo '<li>$var7 es válida porque inicia con una letra.</li>';
+        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
+        echo '</ul>';
+    ?>
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+</html>
