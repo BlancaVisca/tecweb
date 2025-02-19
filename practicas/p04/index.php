8659c6a06c2355e825fcc15f3343a8b2d6f2ca5a
flor <blancavisca05@gmail.com>
2025-02-19 15:24:46 -0600

    Practica 8- formulario productos.html

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -5,232 +5,130 @@
     <title>Práctica 3</title>
 </head>
 <body>
+
     <h2>Ejercicio 1</h2>
     <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
-    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+
     <?php
-        //AQUI VA MI CÓDIGO PHP
-        $_myvar;
-        $_7var;
-        //myvar;       // Inválida
-        $myvar;
-        $var7;
-        $_element1;
-        //$house*5;     // Invalida
-        
         echo '<h4>Respuesta:</h4>';   
-    
-        echo '<ul>';
-        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
-        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
-        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
-        echo '<li>$myvar es válida porque inicia con una letra.</li>';
-        echo '<li>$var7 es válida porque inicia con una letra.</li>';
-        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
-        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
-        echo '</ul>';
+        echo '<ul>
+            <li>$_myvar es válida porque inicia con guión bajo.</li>
+            <li>$_7var es válida porque inicia con guión bajo.</li>
+            <li>myvar es inválida porque no tiene el signo de dólar ($).</li>
+            <li>$myvar es válida porque inicia con una letra.</li>
+            <li>$var7 es válida porque inicia con una letra.</li>
+            <li>$_element1 es válida porque inicia con guión bajo.</li>
+            <li>$house*5 es inválida porque el símbolo * no está permitido.</li>
+        </ul>';
     ?>
-	<h2>Ejercicio 2</h2>
+
+    <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    
-	<?php
-        //AQUI VA MI CÓDIGO PHP
-		$a = "ManejadorSQL";
-		$b = 'MySQL';
-		$c = &$a;
 
-        echo 'Impresión de las variables originales';
-		echo "<br>";
-		echo "\$a = $a"; 
-		echo "<br>";
-		echo "\$b = $b";
-		echo "<br>";
-		echo "\$c = $c";
-		echo "<br>";
-		echo "<br>";
-		$a = "PHP server";
-		$b = &$a;
+    <?php
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
 
-		echo 'Impresión de las variables con modificación';
-		echo "<br>";
-		echo "\$a = $a";
-		echo "<br>";
-		echo "\$b = $b";
-		echo "<br>";
-		echo "\$c = $c";
-		echo "<br>";
-		echo "<br>";
-		echo 'Explicación: Cuando el valor de $a cambia, también se modifica el valor de $c, ya que esta variable guarda la referencia de a, y $b al inicio era una variable independiente, pero al cambiar su valor a la referencia de $a este también se modifica. Quedando las tres variables con el valor que se le proporcione a $a';
-		?>
+        echo '<p>Impresión de las variables originales:</p>';
+        echo '<p>$a = ' . $a . '</p>';
+        echo '<p>$b = ' . $b . '</p>';
+        echo '<p>$c = ' . $c . '</p>';
 
-	<h2>Ejercicio 3</h2>
-	<p>Muestra el contenido de cada variable inmediatamente después de cada asignación, verificar la evolución del tipo de estas variables (imprime todos los componentes de los arreglo):</p>
-		<?php
+        $a = "PHP server";
+        $b = &$a;
 
-		$a = "PHP5";
-		echo "\$a = ";
-		var_dump($a);
-		echo "<br><br>";
+        echo '<p>Impresión de las variables con modificación:</p>';
+        echo '<p>$a = ' . $a . '</p>';
+        echo '<p>$b = ' . $b . '</p>';
+        echo '<p>$c = ' . $c . '</p>';
+    ?>
 
-		$z[] = &$a; 
-		echo "\$z = ";
-		var_dump($z);
-		echo "<br><br>";
+    <h2>Ejercicio 3</h2>
+    <p>Muestra el contenido de cada variable inmediatamente después de cada asignación:</p>
 
-		$b = "5a version de PHP";
-		echo "\$b = ";
-		var_dump($b);
-		echo "<br><br>";
+    <?php
+        $a = "PHP5";
+        echo '<p>$a = ' . var_export($a, true) . '</p>';
 
-		@$c = $b * 10; 
-		echo "\$c = ";
-		var_dump($c);
-		echo "<br><br>";
-		
+        $z[] = &$a;
+        echo '<p>$z = ' . var_export($z, true) . '</p>';
 
-		$a .= $b; 
-		echo "\$a =";
-		var_dump($a);
-		echo "<br><br>";
+        $b = "5a version de PHP";
+        echo '<p>$b = ' . var_export($b, true) . '</p>';
 
-		@$b *= $c; 
-		echo "\$b = ";
-		var_dump($b);
-		echo "<br><br>";
+        @$c = $b * 10;
+        echo '<p>$c = ' . var_export($c, true) . '</p>';
 
-		$z[0] = "MySQL"; 
-		echo "\$z = ";
-		var_dump($z);
-		echo "<br><br>";
+        $a .= $b;
+        echo '<p>$a = ' . var_export($a, true) . '</p>';
 
-		?>
+        @$b *= $c;
+        echo '<p>$b = ' . var_export($b, true) . '</p>';
 
-		<p>Marca error en la inicialización de $c ya que no reconoce su valor ($b*10) ya que $b es un string y lo reconoce como NULL</p>
+        $z[0] = "MySQL";
+        echo '<p>$z = ' . var_export($z, true) . '</p>';
+    ?>
 
+    <h2>Ejercicio 4</h2>
+    <p>Lectura de las variables usando $GLOBALS:</p>
 
-		<h2>Ejercicio 4</h2>
-		<p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
-		la matriz $GLOBALS o del modificador global de PHP.</p>
+    <?php
+        echo '<p>$a = ' . var_export($GLOBALS['a'], true) . '</p>';
+        echo '<p>$z = ' . var_export($GLOBALS['z'], true) . '</p>';
+        echo '<p>$b = ' . var_export($GLOBALS['b'], true) . '</p>';
+        echo '<p>$c = ' . var_export($GLOBALS['c'], true) . '</p>';
+    ?>
 
-		<?php
-		unset($a, $b, $c, $z);
-		$a = "PHP5";
-		$z[] = &$a;
-		$b = "5a version de PHP";
-		@$c = $b * 10;
-		$a .= $b;
-		@$b *= $c;
-		$z[0] = "MySQL";
-		echo "\$a = ";
+    <h2>Ejercicio 5</h2>
+    <p>Dar el valor de las variables $a, $b, $c al final del siguiente script:</p>
 
-		var_dump($GLOBALS['a']);
-		echo "<br><br>";
+    <?php
+        unset($a, $b, $c);
+        $a = "7 personas";
+        $b = (integer) $a;
+        $a = "9E3";
+        $c = (double) $a;
 
-		echo "\$z = ";
-		var_dump($GLOBALS['z']);
-		echo "<br><br>";
+        echo '<p>$a = ' . $a . '</p>';
+        echo '<p>$b = ' . $b . '</p>';
+        echo '<p>$c = ' . $c . '</p>';
+    ?>
 
-		echo "\$b = ";
-		var_dump($GLOBALS['b']);
-		echo "<br><br>";
+    <h2>Ejercicio 6</h2>
 
-		echo "\$c = ";
-		var_dump($GLOBALS['c']);
-		echo "<br><br>";
-		?>
+    <?php
+        $a = "0";
+        $b = "TRUE";
+        $c = FALSE;
+        $d = ($a OR $b);
+        $e = ($a AND $c);
+        $f = ($a XOR $b);
 
-	<h2>Ejercicio 5 </h2>
-	<p>Dar el valor de las variables $a, $b, $c al final del siguiente script:</p>
-	<ul>
-    <li>$a = "7 personas";</li>
-    <li>$b = (integer) $a;</li>
-    <li>$a = "9E3";</li>
-    <li>$c = (double) $a;</li>
-	</ul>
-	<p>Variables finales:</p>
-	<?php
-		unset($a, $b, $c);
-		$a = "7 personas"; 
-		$b = (integer) $a; 
-		$a = "9E3"; 
-		$c = (double) $a; 
+        echo '<p>Valores booleanos de las variables:</p>';
+        echo '<ul>
+            <li>$a = ' . var_export((bool)$a, true) . '</li>
+            <li>$b = ' . var_export((bool)$b, true) . '</li>
+            <li>$c = ' . var_export($c, true) . '</li>
+            <li>$d = ' . var_export($d, true) . '</li>
+            <li>$e = ' . var_export($e, true) . '</li>
+            <li>$f = ' . var_export($f, true) . '</li>
+        </ul>';
+    ?>
 
-		echo "\$a = $a <br>";
-		echo "\$b = $b <br>";
-		echo "\$c = $c <br>";
-	?>
-<h2>Ejercicio 6</h2>
+    <h2>Ejercicio 7</h2>
+    <ul>
+        <li><strong>Versión de Apache y PHP:</strong> <?php echo htmlentities($_SERVER['SERVER_SOFTWARE']); ?></li>
+        <li><strong>Sistema Operativo del Servidor:</strong> <?php echo htmlentities(php_uname()); ?></li>
+        <li><strong>Idioma del Navegador (Cliente):</strong> <?php echo htmlentities($_SERVER['HTTP_ACCEPT_LANGUAGE']); ?></li>
+    </ul>
 
+    <p>
+        <a href="https://validator.w3.org/markup/check?uri=referer">
+            <img src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" />
+        </a>
+    </p>
 
-		<?php
-		$a = "0";          
-		$b = "TRUE";       
-		$c = FALSE;        
-		$d = ($a OR $b);   
-		$e = ($a AND $c);  
-		$f = ($a XOR $b);  
-
-		echo "Valores booleanos de las variables:";
-		echo "<ul>";
-		echo "<li>\$a = "; var_dump((bool)$a); 
-		echo "<li>\$b = "; var_dump((bool)$b); 
-		echo "<li>\$c = "; var_dump($c);
-		echo "<li>\$d = "; var_dump($d); 
-		echo "<li>\$e = "; var_dump($e);
-		echo "<li>\$f = "; var_dump($f); 
-		echo "</ul>";
-
-		
-		echo "Transformando valores booleanos en texto usando var_export:";
-		echo "<br>\$c = " . var_export($c, true) . "<br>";
-		echo "\$e = " . var_export($e, true) . "<br>";
-		?>
-
-	<h2>Ejercicio 7</h2>
-	<ul>
-    <li><strong>Versión de Apache y PHP:</strong> <?php echo $_SERVER['SERVER_SOFTWARE']; ?></li>
-    <li><strong>Sistema Operativo del Servidor:</strong> <?php echo php_uname(); ?></li>
-    <li><strong>Idioma del Navegador (Cliente):</strong> <?php echo $_SERVER['HTTP_ACCEPT_LANGUAGE']; ?></li>
-	</ul>
-
-
-<!-- Code injected by live-server -->
-<script>
-	// <![CDATA[  <-- For SVG support
-	if ('WebSocket' in window) {
-		(function () {
-			function refreshCSS() {
-				var sheets = [].slice.call(document.getElementsByTagName("link"));
-				var head = document.getElementsByTagName("head")[0];
-				for (var i = 0; i < sheets.length; ++i) {
-					var elem = sheets[i];
-					var parent = elem.parentElement || head;
-					parent.removeChild(elem);
-					var rel = elem.rel;
-					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
-						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
-						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
-					}
-					parent.appendChild(elem);
-				}
-			}
-			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
-			var address = protocol + window.location.host + window.location.pathname + '/ws';
-			var socket = new WebSocket(address);
-			socket.onmessage = function (msg) {
-				if (msg.data == 'reload') window.location.reload();
-				else if (msg.data == 'refreshcss') refreshCSS();
-			};
-			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
-				console.log('Live reload enabled.');
-				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
-			}
-		})();
-	}
-	else {
-		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
-	}
-	// ]]>
-</script>
 </body>
 </html>
---
+++ practicas/p08-base/set_producto_v2.php
@@ -0,0 +1,41 @@
+<?php
+/** SE CREA EL OBJETO DE CONEXION */
+@$link = new mysqli('localhost', 'root', 'blancaflor', 'marketzone');	
+
+/** comprobar la conexión */
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+}
+$nombre   = $_POST['nombre'];
+$marca    = $_POST['marca'];
+$modelo   = $_POST['modelo'];
+$precio   = $_POST['precio'];
+$detalles = $_POST['detalles'];
+$unidades = $_POST['unidades'];
+$imagen   = $_POST['img'];
+
+
+
+/** Verificar si el producto ya existe (nombre, modelo y marca) */
+$sql_check = "SELECT * FROM productos WHERE nombre = '{$nombre}' AND marca = '{$marca}' AND modelo = '{$modelo}'";
+$result = $link->query($sql_check);
+
+if ($result->num_rows > 0) {
+    echo 'El producto ya está registrado con el mismo nombre, modelo y marca.';
+} else {
+    /** Insertar producto si no existe */
+    $sql_insert = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+
+    if ($link->query($sql_insert)) {
+        echo 'Producto insertado con ID: ' . $link->insert_id;
+    } else {
+        echo 'El Producto no pudo ser insertado =(';
+    }
+}
+
+
+
+$link->close();
+?>
